bb9865a74b57dadcf3dfe50521777423fdc445f2
ColinGilbert <colingilbert86@gmail.com>
2015-02-04 15:51:53 -0600

    Begin integration - properly

---
+++ Callbacks.h
@@ -0,0 +1,13 @@
+extern "C" {
+	void ExitApp();
+	void OnStart();
+	void OnStop();
+	void OnDestroy();
+	void OnDrawFrame();
+	void OnTimer( float delta );
+	void OnKey( int code, bool state );
+	void OnMouseDown( int btn, float x, float y);
+	void OnMouseMove( float x, float y );
+	void OnMouseUp( int btn, i float x, float y );
+	void GenerateTicks();
+}
