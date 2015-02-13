3956fafdb068ac4e95211977f37f6b8ca28d48f1
ColinGilbert <colingilbert86@gmail.com>
2015-02-13 05:00:42 -0600

    Continue bringing back old engine

---
+++ android/jni/Application.mk
@@ -0,0 +1,8 @@
+APP_OPTIM := release
+APP_ARM_MODE := arm
+APP_ABI := armeabi-v7a
+APP_PLATFORM := android-18
+APP_CPPFLAGS := c++11 -frtti -fexceptions
+APP_CFLAGS := c99
+APP_STL := gnustl_static
+NDK_TOOLCHAIN := clangi
