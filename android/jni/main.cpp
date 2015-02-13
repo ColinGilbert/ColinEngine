645892e9f57fc2e9b7539b160106a01624602a87
ColinGilbert <colingilbert86@gmail.com>
2015-02-13 06:09:25 -0600

    FIXED THE BUILD!

--- android/jni/main.cpp
+++ android/jni/main.cpp
@@ -14,7 +14,7 @@
 #include <android/sensor.h>
 #include <android/log.h>
 #include <android_native_app_glue.h>
-//#include <android/native-window.h>
+//#include <android/native_window.h>
 #include <time.h>
 
 #include <iostream>
